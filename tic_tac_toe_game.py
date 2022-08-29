2edc02d3b2e3ff77c0090b8196fefb315e642dc4
Prachi Srivastava <87762435+PrachiSrivastava2709@users.noreply.github.com>
2022-08-29 23:12:37 +0530

    Added a UnbeatableComputerPlayer (minimax algo)

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -37,5 +37,46 @@ class UserPlayer(Player):
 
         return spot
 
+class UnbeatableComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
 
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            spot = random.choice(game.available_moves())
+        else:
+            spot = self.minimax(game, self.letter)['position']
+        return spot
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'
+
+        # first we want to check if the previous move is a winner
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_spots() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_spots() + 1)}
+        elif not state.empty_spots():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # each score should maximize
+        else:
+            best = {'position': None, 'score': math.inf}  # each score should minimize
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
+
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move  # this represents the move optimal next move
+
+            if player == max_player:  # X is max player
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
 
