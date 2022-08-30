7d2f16619634c2869c013388b33a1c49a2919ece
Prachi Srivastava <87762435+PrachiSrivastava2709@users.noreply.github.com>
2022-08-30 15:33:46 +0530

    Update tic_tac_toe_game.py

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -53,9 +53,12 @@ class UnbeatableComputerPlayer(Player):
         other_player = 'O' if player == 'X' else 'X'
 
         # first we want to check if the previous move is a winner
+        # as the other player is also playing optimally, this will get executed
+        # Assumption of Minimax Algorithm that both players are playing "optimally"
         if state.current_winner == other_player:
             return {'position': None, 'score': 1 * (state.num_empty_spots() + 1) if other_player == max_player else -1 * (
                         state.num_empty_spots() + 1)}
+        #this elif condition can be commented out
         elif not state.empty_spots():
             return {'position': None, 'score': 0}
 
@@ -65,17 +68,17 @@ class UnbeatableComputerPlayer(Player):
             best = {'position': None, 'score': math.inf}  # each score should minimize
         for possible_move in state.available_moves():
             state.make_move(possible_move, player)
-            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
+            sim_score = self.minimax(state, other_player)  # simulate a game after making that move #goes uptil next level and not full depth #recursion
 
             # undo move
             state.board[possible_move] = ' '
             state.current_winner = None
             sim_score['position'] = possible_move  # this represents the move optimal next move
 
-            if player == max_player:  # X is max player
+            if player == max_player:  # X is the AI
                 if sim_score['score'] > best['score']:
                     best = sim_score
-            else:
+            else:                     # O is the AI
                 if sim_score['score'] < best['score']:
                     best = sim_score
         return best
